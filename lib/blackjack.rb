7df3126af9202a45c0932d984d254ed896e51cc9
Tarun Jadhwani <tarun.jadhwani@practo.com>
2017-01-28 19:20:23 +0530

    project structure

---
+++ lib/blackjack.rb
@@ -0,0 +1,5 @@
+require "blackjack/version"
+
+module Blackjack
+  # Your code goes here...
+end
---
+++ spec/blackjack_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Blackjack do
+  it 'has a version number' do
+    expect(Blackjack::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
